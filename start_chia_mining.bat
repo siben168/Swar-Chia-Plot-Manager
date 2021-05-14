9b91a7232f2e2269cce14167462a68686afa88b9
siben168 <siben168@gmail.com>
2021-05-14 12:48:43 +0800

    add files

---
+++ start_chia_mining.bat
@@ -0,0 +1,7 @@
+C:
+cd C:\workspace\pyenv\Scripts
+CALL activate
+cd C:\workspace\chia\Original-Chia-Plot-Manager
+python manager.py start
+python manager.py view
+pause
---
+++ view_chia_mining.bat
@@ -0,0 +1,6 @@
+C:
+cd C:\workspace\pyenv\Scripts
+CALL activate
+cd C:\workspace\chia\Original-Chia-Plot-Manager
+python manager.py view
+pause
